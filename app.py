0964ce19ceca60bd7107da0a6818797fbe893478
Oussama Darrazi <nostra.darrazi2002@gmail.com>
2024-05-23 16:18:12 +0100

    main page

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
-
+from flask import render_template
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/", methods = ["GET","POST"])
+def index():
+    return render_template("index.html")
 
-
+@app.route("/display", methods = ["GET","POST"])
+def display():
+    return render_template("result.html")
 
 
 if __name__ == '__main__':
